573d6574274cede3582af05b23ab9d1add67e608
TessierV <5628@holbertonstudents.com>
2023-01-23 01:59:18 -0800

    change the return  value

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    number = 0
     for i in range(0, x):
-        number = 0
         try:
             print('{}'.format(my_list[i]), end="")
             number += 1
