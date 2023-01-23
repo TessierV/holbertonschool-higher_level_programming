b5fa1f02c1c8ec767beeb4ffc31356c93b577de4
TessierV <5628@holbertonstudents.com>
2023-01-23 07:29:07 -0800

    9 script

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     max = my_list[0]
-    if len(my_list) != 0:
+
+    if len(my_list) == 0:
+        return None
+    else:
         for i in my_list:
             if i > max:
                 max = i
         return max
-    elif len(my_list) == 0:
-        return None
+
