3bf63509956a600cded00f89d3f1ee1887aa8bd1
TessierV <5628@holbertonstudents.com>
2023-01-23 02:22:57 -0800

    test checker

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -5,7 +5,7 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end="")
             number += 1
-        except (RuntimeError, TypeError, NameError):
+        except (RuntimeError, TypeError, ValueError):
             pass
     print()
     return number
