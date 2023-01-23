9867878fc938a4282e9b04c61de21de4d038ea69
TessierV <5628@holbertonstudents.com>
2023-01-23 05:34:24 -0800

    test

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -4,5 +4,5 @@ def print_matrix_integer(matrix=[[]]):
         for y in x:
             print('{:d}'.format(y), end="")
             if y != x[-1]:
-                print(end =" ")
+                print(end=" ")
         print()
