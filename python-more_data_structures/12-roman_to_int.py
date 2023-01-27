328e725601cb35e2accd4f7c9da16b719afc7189
TessierV <5628@holbertonstudents.com>
2023-01-27 08:19:26 -0800

    +1

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,7 @@
 def roman_to_int(roman_string):
     result = 0
     numList = []
-    dict = {'V': 1, 'V': 5, 'X': 10,
+    dict = {'I': 1, 'V': 5, 'X': 10,
             'L': 50, 'C': 100, 'D': 500,
             'M': 1000}
     if roman_string == "" or not isinstance(roman_string, str):
