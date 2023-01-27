5b911809c1387c889495bb53fed8db44c50eedd9
TessierV <5628@holbertonstudents.com>
2023-01-27 08:21:34 -0800

    +1

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -5,18 +5,18 @@ def roman_to_int(roman_string):
     dict = {'I': 1, 'V': 5, 'X': 10,
             'L': 50, 'C': 100, 'D': 500,
             'M': 1000}
-    if roman_string == "" or not isinstance(roman_string, str):
+    if roman_string or isinstance(roman_string, str):
+        for romanC in roman_string:
+            for convertKey in dict:
+                if romanC == convertKey:
+                    numList.append(dict[convertKey])
+        char = 0
+        for i in numList:
+            if (i > char):
+                result -= char
+            else:
+                result += char
+            char = i
+        result += char
         return result
-    for romanC in roman_string:
-        for convertKey in dict:
-            if romanC == convertKey:
-                numList.append(dict[convertKey])
-    char = 0
-    for i in numList:
-        if (i > char):
-            result -= char
-        else:
-            result += char
-        char = i
-    result += char
     return result
