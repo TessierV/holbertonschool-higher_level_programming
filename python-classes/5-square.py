8b34446ff314de44a7c8a3c70442c91b28d7691e
TessierV <5628@holbertonstudents.com>
2023-01-31 03:36:48 -0800

    5-square.py

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -13,7 +13,7 @@ class Square:
 
     def area(self):
         return self.__size*self.__size
-    
+
     @property
     def size(self):
         return self.__size
@@ -25,10 +25,10 @@ class Square:
             raise TypeError('size must be an integer')
         if value < 0:
             raise ValueError('size must be >= 0')
-    
+
     def my_print(self):
         self.my_print = self
-        if self == 0:
+        if self.__size == 0:
             print('')
         else:
             for i in range (0, self.__size):
