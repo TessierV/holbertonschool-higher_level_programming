d8fec00282ca8571b5c4726ee8643cf17a9cc95a
TessierV <5628@holbertonstudents.com>
2023-02-02 03:33:10 -0800

    pep

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -42,7 +42,7 @@ class Rectangle:
             return 2*(self.__width + self.__height)
 
     def __str__(self):
-        rec= ""
+        rec = ""
         if self.__width == 0 or self.__height == 0:
             return rec
         else:
