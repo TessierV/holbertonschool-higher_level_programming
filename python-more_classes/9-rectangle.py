fdaccb3779199f65ed6633376f74acc2a0f97f3f
TessierV <5628@holbertonstudents.com>
2023-02-02 04:47:31 -0800

    forgot the h on method

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -72,7 +72,7 @@ class Rectangle:
             raise TypeError('rect_2 must be an instance of Rectangle')
         if type(rect_1) == type(rect_2):
             return rect_1
-
-    @classmetod
+    
+    @classmethod
     def square(cls, size=0):
         return cls(size, size)
