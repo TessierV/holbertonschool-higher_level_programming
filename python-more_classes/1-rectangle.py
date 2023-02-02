d648cf5de2a3590a60d9724b67533855b4bcaaf6
TessierV <5628@holbertonstudents.com>
2023-02-02 01:50:02 -0800

    script 1

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,1 +1,33 @@
 #!/usr/bin/python3
+'''Rectangle'''
+
+
+class Rectangle:
+    '''class: Rectangle.'''
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+    
+    @property
+    def width(self):
+        return self.__width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        self.__width = value
+        if type(self.__width) is not int:
+            raise TypeError('width must be an integer')
+        if self.__width < 0:
+            raise ValueError('width must be >= 0')
+    
+    @height.setter
+    def height(self, value):
+        self.__height = value
+        if type(self.__height) is not int:
+            raise TypeError('height must be an integer')
+        if self.__height < 0:
+            raise ValueError('height must be >= 0')
