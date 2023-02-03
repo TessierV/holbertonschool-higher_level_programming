b4cc0673aadbd20bcb4651fada1e5abe679f3d07
TessierV <5628@holbertonstudents.com>
2023-02-03 05:14:14 -0800

    space between def

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -8,20 +8,10 @@ max_integer = __import__('6-max_integer').max_integer
 class TestMaxInteger(unittest.TestCase):
     def test_max_end(self):
         self.assertAlmostEqual(max_integer([1, 2, 3]), 3)
-    
-    def test_max_beguinnning(self):
         self.assertAlmostEqual(max_integer([3, 2, 1]), 3)
-    
-    def test_max_middle(self):
         self.assertAlmostEqual(max_integer([1, 3, 2]), 3)
-    
-    def test_one_negativ(self):
         self.assertAlmostEqual(max_integer([-1, 2, 3]), 3)
-    
-    def test_all_negativ(self):
         self.assertAlmostEqual(max_integer([-1, -2, -3]), -3)
-    
-    def test_one_negativ(self):
         self.assertAlmostEqual(max_integer([]), None)
 
 if __name__ == '__main__':
