448f3a30455e030b00db91d7524b27573e015d29
TessierV <5628@holbertonstudents.com>
2023-02-03 01:34:16 -0800

    adding the try / except even if it was working my way

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -9,5 +9,5 @@ def add_integer(a, b=98):
     except:
         if type(a) is not int and type(a) is not float:
             raise TypeError('a must be an integer')
-        else:
+        if type(b) is not int and type(b) is not float:
             raise TypeError('b must be an integer')
