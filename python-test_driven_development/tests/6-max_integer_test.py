b0f78c9341679c8619bd6df8800e8bf405e8168a
TessierV <5628@holbertonstudents.com>
2023-02-03 05:46:17 -0800

    check with def and with one negative

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -16,7 +16,7 @@ class TestMaxInteger(unittest.TestCase):
         self.assertAlmostEqual(max_integer([1, 3, 2]),3)
 
     def test_one_negativ(self):
-        self.assertAlmostEqual(max_integer([1, -2, 3]), 3)
+        self.assertAlmostEqual(max_integer([-1, 0, 2, 3]), 3)
 
 if __name__ == '__main__':
     unittest.main()
