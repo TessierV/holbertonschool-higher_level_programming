6a9ec498dd52b68239cb9aaa17b9a5f0b749d76c
TessierV <5628@holbertonstudents.com>
2023-02-03 03:09:19 -0800

    counter the elif

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -5,10 +5,12 @@ def print_square(size):
     """square with the character #"""
     if size < 0:
         raise ValueError('size must be >= 0')
-    if isinstance(size, int) is True and size >= 0:
+    if isinstance(size, int) is False:
+        raise TypeError('size must be an integer')
+    if isinstance(size, float) is True and size < 0: 
+        raise TypeError('size must be an integer')
+    else:
         for i in range(size):
             for j in range(size):
                 print("#", end="")
             print('')
-    else: 
-        raise TypeError('size must be an integer')
