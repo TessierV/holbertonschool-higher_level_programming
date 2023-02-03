2273c3ecdd7b9d05bf7d8c38ec3de5aa844009b8
TessierV <5628@holbertonstudents.com>
2023-02-03 05:35:56 -0800

    check wich doesnt work

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -10,6 +10,9 @@ class TestMaxInteger(unittest.TestCase):
         self.assertAlmostEqual(max_integer([1, 2, 3]), 3)
         self.assertAlmostEqual(max_integer([3, 2, 1]), 3)
         self.assertAlmostEqual(max_integer([1, 3, 2]),3)
+        self.assertEqual(max_integer([1, -2, 3]), 3)
+        self.assertEqual(max_integer([-1, -2, -3]), -3)
+        self.assertEqual(max_integer([]), None)
 
 if __name__ == '__main__':
     unittest.main()
