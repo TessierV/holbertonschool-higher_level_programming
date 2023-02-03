d75c5269ce13e3fa2201ae5e20c54e63f80b3f3a
TessierV <5628@holbertonstudents.com>
2023-02-03 03:16:43 -0800

    order if

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -5,9 +5,7 @@ def print_square(size):
     """square with the character #"""
     if isinstance(size, int) is False:
         raise TypeError('size must be an integer')
-    elif isinstance(size, int) and size < 0:
+    if isinstance(size, int) and size < 0:
         raise ValueError('size must be >= 0')
     for i in range(size):
-        for j in range(size):
-            print("#", end="")
-        print('')
+        print("#" *size)
