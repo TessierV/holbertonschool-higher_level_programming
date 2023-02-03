e84ee991f18bc5c1e7578dcec762c4f752b07d2e
TessierV <5628@holbertonstudents.com>
2023-02-03 05:18:34 -0800

    adding back binbah

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -1,4 +1,4 @@
-import unittest
+#!/usr/bin/python3
 """test for max_integer
 """
 import unittest
@@ -8,10 +8,20 @@ max_integer = __import__('6-max_integer').max_integer
 class TestMaxInteger(unittest.TestCase):
     def test_max_end(self):
         self.assertAlmostEqual(max_integer([1, 2, 3]), 3)
+    
+    def test_max_beguinnning(self):
         self.assertAlmostEqual(max_integer([3, 2, 1]), 3)
+    
+    def test_max_middle(self):
         self.assertAlmostEqual(max_integer([1, 3, 2]), 3)
+    
+    def test_one_negativ(self):
         self.assertAlmostEqual(max_integer([-1, 2, 3]), 3)
+    
+    def test_all_negativ(self):
         self.assertAlmostEqual(max_integer([-1, -2, -3]), -3)
+    
+    def test_one_negativ(self):
         self.assertAlmostEqual(max_integer([]), None)
 
 if __name__ == '__main__':
