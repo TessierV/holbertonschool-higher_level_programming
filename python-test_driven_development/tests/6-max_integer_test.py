445028d5c5811cc1b921188064fc9d8290dad2d3
TessierV <5628@holbertonstudents.com>
2023-02-03 05:35:07 -0800

    check wich doesnt work

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -6,11 +6,10 @@ max_integer = __import__('6-max_integer').max_integer
 
 
 class TestMaxInteger(unittest.TestCase):
-
     def test_results(self):
-        self.assertEqual(max_integer([1, 2, 3]), 3)
-        self.assertEqual(max_integer([3, 2, 1]), 3)
-        self.assertEqual(max_integer([1, 3, 2]), 3)
-        self.assertEqual(max_integer([-1, 2, 3]), 3)
-        self.assertEqual(max_integer([-1, -2, -3]), -3)
-        self.assertEqual(max_integer([]), None)
+        self.assertAlmostEqual(max_integer([1, 2, 3]), 3)
+        self.assertAlmostEqual(max_integer([3, 2, 1]), 3)
+        self.assertAlmostEqual(max_integer([1, 3, 2]),3)
+
+if __name__ == '__main__':
+    unittest.main()
