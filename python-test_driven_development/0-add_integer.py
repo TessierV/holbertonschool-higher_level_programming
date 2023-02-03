8d261e2063c479bfc10abde92bc8cd924dcfe695
TessierV <5628@holbertonstudents.com>
2023-02-03 01:42:16 -0800

    test

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-'''add_integer'''
-
+"""
+    function to add two int
+"""
 
 def add_integer(a, b=98):
     '''function who add two integer'''
     try:
-        result = a + b
-        return int(result)
+        return int(a) + int(b)
     except:
         if type(a) is not int and type(a) is not float:
             raise TypeError('a must be an integer')
-        else:
+        if type(b) is not int and type(b) is not float:
             raise TypeError('b must be an integer')
