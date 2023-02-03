83d89f707c8bf4800efb99929bc3adeed46c864c
TessierV <5628@holbertonstudents.com>
2023-02-03 01:26:15 -0800

    test if issu is main function checker

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
-'''add_integer'''
+"""
+    function to add two int
+"""
 
 
 def add_integer(a, b=98):
-    '''function who add two integer'''
-    if type(a) is not int and type(a) is not float:
-        raise TypeError('a must be an integer')
-    if type(b) is not int and type(b) is not float:
-        raise TypeError('b must be an integer')
-    else:
-        return int(a) + int(b)
+    """ function to add two integers """
+    try:
+        result = a + b
+        return int(result)
+    except:
+        if type(a) is not int:
+            raise TypeError("a must be an integer")
+        else:
+            raise TypeError("b must be an integer")
