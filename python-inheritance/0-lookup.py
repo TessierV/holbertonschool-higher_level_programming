2827632f1693142c7b11d2e0d0635de16a77ce93
TessierV <5628@holbertonstudents.com>
2023-02-06 12:20:14 -0800

    fix pep8

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def lookup(obj):
-        """
-        returns the list of avaiable attributes and methods of an objects
-        """
-        return dir(obj)
+    """
+    Returns the list of available attributes and methods of an object
+    """
+    return dir(obj)
