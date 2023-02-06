4a64859cd23765333d6e81312faa2870111940df
TessierV <5628@holbertonstudents.com>
2023-02-06 14:16:32 -0800

    script 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Creating an empty class: BaseGeometry"""
+
+
+class BaseGeometry:
+    """
+    Creating an empty class: BaseGeometry
+    """
+    def area(self):
+        raise Exception ("area() is not implemented")
