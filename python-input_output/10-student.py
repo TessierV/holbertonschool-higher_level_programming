2237a9c4cdc6731923f365f9e7905dfb991e98e0
TessierV <5628@holbertonstudents.com>
2023-02-07 14:31:15 -0800

    test

---
+++ python-input_output/10-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import json
+"""Studient"""
+
+
+class Student:
+    """
+    Write a class Student that defines a student.
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        list = {}
+        if attrs is None:
+            return self.__dict__
+        else:
+            for i in attrs:
+                for i in self.__dict__:
+                    list[i] = self.__dict__[i]
+                return list
