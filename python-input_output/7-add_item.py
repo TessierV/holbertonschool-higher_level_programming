30cb2a0fef6b856d47b3e04784d05cf0aae2aa29
TessierV <5628@holbertonstudents.com>
2023-02-07 12:22:46 -0800

    test with doc

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
-""" This json import """
+""" This sys import """
 import sys
 
-"""
-Write a script that adds all arguments
-to a Python list, and then save them to a file:
-"""
 
 file = '6-load_from_json_file.py'
 save_to_json_file = __import__('5-save_to_json_file.py').save_to_json_file
