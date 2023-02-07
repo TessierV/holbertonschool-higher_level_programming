4122827c4c6542ddce1e66b97806ac2a3ed62a4f
TessierV <5628@holbertonstudents.com>
2023-02-07 12:20:00 -0800

    test with doc

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
 """ This json import """
-
-
 import sys
-""" Function """
-
-file = '6-load_from_json_file.py'
-save_to_json_file = __import__('5-save_to_json_file.py').save_to_json_file
-load_from_json_file = __import__('file').load_from_json_file
 
 """
 Write a script that adds all arguments
 to a Python list, and then save them to a file:
 """
+
+
+file = '6-load_from_json_file.py'
+save_to_json_file = __import__('5-save_to_json_file.py').save_to_json_file
+load_from_json_file = __import__('file').load_from_json_file
+
 try:
     for i in sys.argv[1:]:
         list.append(i)
