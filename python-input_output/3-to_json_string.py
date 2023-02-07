9f57fe2071a71b154d887955fbad1f761197272c
TessierV <5628@holbertonstudents.com>
2023-02-07 11:38:27 -0800

    json script1

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import json
+""" Function """
+
+
+def to_json_string(my_obj):
+    """
+    Write a function that reads a text file (UTF8) and prints it to stdout:
+    """
+    return json.dumps(my_obj)
