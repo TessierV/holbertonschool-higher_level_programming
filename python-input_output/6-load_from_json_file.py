e3bdced5b727a4ff1d3e0a7aba587ed8602df11b
TessierV <5628@holbertonstudents.com>
2023-02-07 11:54:02 -0800

    script 6

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" This json import """
+
+import json
+""" Function """
+
+
+def load_from_json_file(filename):
+    """
+    Write a function that creates an
+    Object from a “JSON file”:
+    """
+    with open(filename, 'r', encoding="utf-8") as file:
+        return json.load(file)
