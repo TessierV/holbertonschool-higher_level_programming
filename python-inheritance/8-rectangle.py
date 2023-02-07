11be21c636318fee03af57facf03c9869109ad43
TessierV <5628@holbertonstudents.com>
2023-02-07 10:19:18 -0800

    issu with one variable

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -11,5 +11,5 @@ class Rectangle(BaseGeometry):
     def __init__(self, width, height):
         self.integer_validator("width", width)
         self.__width = width
-        self.integer_validator("height", width)
+        self.integer_validator("height", height)
         self.__height = height
