fed21b5c24b99ea93320c8e2185d05bb507b09fc
TessierV <5628@holbertonstudents.com>
2023-02-07 12:17:16 -0800

    test ?

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" This json import """
+
+import sys
+import json
+""" Function """
+
+file = '6-load_from_json_file.py'
+save_to_json_file = __import__('5-save_to_json_file.py').save_to_json_file
+load_from_json_file = __import__('file').load_from_json_file
+
+"""
+Write a script that adds all arguments
+to a Python list, and then save them to a file:
+"""
+
+try:
+    for i in sys.argv[1:]:
+        list.append(i)
+    list = load("add_item.json")
+except Exception:
+    list = []
+save(list, "add_item.json")
