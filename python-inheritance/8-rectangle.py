2cf19082fd586747e19833a3481b81fa1fde85bb
TessierV <5628@holbertonstudents.com>
2023-02-07 10:18:14 -0800

    documentation

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
-
 """Creating a class: Rectangle"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
     """def __init__"""
+
     def __init__(self, width, height):
         self.integer_validator("width", width)
         self.__width = width
         self.integer_validator("height", width)
         self.__height = height
-
-Rectangle = __import__('8-rectangle').Rectangle
