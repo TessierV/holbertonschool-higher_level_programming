15ccef10611d24a9ddc62effc04b801721da30c6
TessierV <5628@holbertonstudents.com>
2023-02-07 11:06:24 -0800

    test 0

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""" Function """
+
+
+def read_file(filename=""):
+    """
+    Write a function that reads a text file (UTF8) and prints it to stdout:
+    """
+    with open(filename, encoding="utf-8")  as my_file:
+        print(my_file.read())
