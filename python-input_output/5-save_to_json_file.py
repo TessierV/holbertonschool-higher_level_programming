e0bf1fb34decb7a5c8bfb61bd8247e69ce2972bf
TessierV <5628@holbertonstudents.com>
2023-02-08 03:51:00 -0800

    change value of 5

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -10,5 +10,5 @@ def save_to_json_file(my_obj, filename):
     Write a function that writes an Object
     to a text file, using a JSON representation:
     """
-    with open(filename, 'aw', encoding="utf-8") as file:
+    with open(filename, 'w', encoding="utf-8") as file:
         file.write(json.dumps(my_obj))
--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-""" fgdfgdfgd"""
 import sys
-""" Function sdfsdfsd """
+""" Function """
 
 
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save = __import__('5-save_to_json_file').save_to_json_file
+load = __import__('6-load_from_json_file').load_from_json_file
 
 list = []
 try:
-    save_to_json_file(load_from_json_file("add_item.json") + sys.argv[1:], "add_item.json")
+    save(load("add_item.json") + sys.argv[1:], "add_item.json")
 except:
-    save_to_json_file(sys.argv[1:], "add_item.json")
+    save(sys.argv[1:], "add_item.json")
