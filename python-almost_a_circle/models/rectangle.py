7dbdea42a4026d4461f87fc8d6f3b5b95e4dda60
TessierV <5628@holbertonstudents.com>
2023-02-13 10:19:52 -0800

    pep8

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -9,7 +9,6 @@ from models.base import Base
 class Rectangle(Base):
     """Class: Rectangle"""
 
-
     def __init__(self, width, height, x=0, y=0, id=None):
         """Class constructor"""
         super().__init__(id)
