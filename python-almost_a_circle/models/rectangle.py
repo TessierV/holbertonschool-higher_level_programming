5bf01018ceb30be3cbd33dbafa3bbb08ffd87e20
TessierV <5628@holbertonstudents.com>
2023-02-13 10:31:03 -0800

    smaller print

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -88,5 +88,5 @@ class Rectangle(Base):
 
     def __str__(self):
         """returns [Rectangle] (<id>) <x>/<y> - <width>/<height>"""
-
-        return ('[Rectangle] ({}) {}/{} - {}/{}'.format(self.id, self.__x, self.__y, self.__width, self.__height))
+        print_wh = "[Rectangle] ({}) {}/{} - {}/{}"
+        return (print_wh.format(self.id, self.__x, self.__y, self.__width, self.__height))
