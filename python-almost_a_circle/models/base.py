b6f131d31fc7acb23043d2f275315e0965a766e8
TessierV <5628@holbertonstudents.com>
2023-02-14 10:17:55 -0800

    script 14

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -19,7 +19,7 @@ class Base:
 
     def to_json_string(list_dictionaries):
         """ json return the dictionaries"""
-        if list_dictionaries is None or list_dictionaries == []:
+        if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
