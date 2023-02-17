13d9fdda9070a3f96a42911cd2c3b2b5461ffe65
TessierV <5628@holbertonstudents.com>
2023-02-17 09:10:22 -0800

    test

--- python-almost_a_circle/tests/test_rectangle.py
+++ python-almost_a_circle/tests/test_rectangle.py
@@ -9,14 +9,15 @@ class testbase(unittest.TestCase):
     """Class test rectangle"""
 
     """Tesst of Rectangle"""
-    """Test of Rectangle() for assigning automatically an ID exists"""
+    """Test of Rectangle with not integers"""
 
     def test_invalid_arg_type(self):
         """Invalid argument type on instantiation"""
         self.assertRaises(TypeError, Rectangle, 1, "2")
         self.assertRaises(TypeError, Rectangle, "1", 2)
         self.assertRaises(TypeError, Rectangle, "1", "2")
-        self.assertRaises(TypeError, Rectangle, 1, ... , "3")
+        self.assertRaises(TypeError, Rectangle, 1, 2, "3")
+        self.assertRaises(TypeError, Rectangle, 1, 2, 3, "4")
 
 if __name__ == '__main__':
     unittest.main()
