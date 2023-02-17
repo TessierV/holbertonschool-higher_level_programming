f993179f6c3269433b4bec6f80552e2608282dca
TessierV <5628@holbertonstudents.com>
2023-02-17 09:27:29 -0800

    test

--- python-almost_a_circle/tests/test_square.py
+++ python-almost_a_circle/tests/test_square.py
@@ -7,9 +7,13 @@ from models.square import Square
 
 class testbase(unittest.TestCase):
     """Class test Square"""
-
-    """Tesst of Square"""
-    """Test of Square with not integers"""
+    def test_invalid_arg_type(self):
+        self.assertRaises(ValueError, Square, -1, 2)
+        self.assertRaises(ValueError, Square, 1, -2)
+        self.assertRaises(ValueError, Square, 0, 2)
+        self.assertRaises(ValueError, Square, 1, 0)
+        self.assertRaises(ValueError, Square, 1, 2, -3)
+        self.assertRaises(ValueError, Square, 1, 2, 3, -4)
 
 
 if __name__ == '__main__':
