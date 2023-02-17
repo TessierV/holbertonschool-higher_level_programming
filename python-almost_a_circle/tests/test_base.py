b3032e66565bd8987efe82b9939c0716324fba15
TessierV <5628@holbertonstudents.com>
2023-02-17 07:51:03 -0800

    test

--- python-almost_a_circle/tests/test_base.py
+++ python-almost_a_circle/tests/test_base.py
@@ -10,22 +10,27 @@ from models.base import Base
 class testbase(unittest.TestCase):
     """Class test base"""
 
+    """Tesst of Base"""
     """Test of Base() for assigning automatically an ID exists"""
     def test_id(self):
         base = Base()
         self.assertEqual(base.id, 1)
-    """Test of Base() for assigning automatically an ID + 1 of the previous exists"""
-    """def test_id_prev(self):
-        base = Base()
-        self.assertEqual(base.id, 2)"""
+
     """Test of Base(89) saving the ID passed exists"""
     def test_id_pass(self):
         base = Base(89)
         self.assertEqual(base.id, 89)
-    """Test of Base.to_json_string([]) exists"""
+
+    """Test of Base.to_json"""
+    """"All test validate from base.to_json"""
     def test_none(self):
         base = Base()
         self.assertEqual(base.to_json_string([]), '[]')
+
+    """Base.from_json_string"""
+    def test_from_none(self):
+        base = Base()
+        self.assertEqual(base.from_json_string('[]'), [])
 
 if __name__ == '__main__':
     unittest.main()
