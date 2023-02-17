d9937b779cddb4d63c4b722c3783233360358cf4
TessierV <5628@holbertonstudents.com>
2023-02-17 07:38:54 -0800

    test

--- python-almost_a_circle/tests/test_base.py
+++ python-almost_a_circle/tests/test_base.py
@@ -15,14 +15,14 @@ class testbase(unittest.TestCase):
         base = Base()
         self.assertEqual(base.id, 1)
     """Test of Base() for assigning automatically an ID + 1 of the previous exists"""
-    def test_id_prev(self):
+    """def test_id_prev(self):
         base = Base()
-        self.assertEqual(base.id, 2)
+        self.assertEqual(base.id, 2)"""
     """Test of Base(89) saving the ID passed exists"""
     def test_id_pass(self):
         base = Base(89)
         self.assertEqual(base.id, 89)
-    """Test of Base.to_json_string(None) exists"""
+    """Test of Base.to_json_string([]) exists"""
     def test_none(self):
         base = Base()
         self.assertEqual(base.to_json_string([]), '[]')
