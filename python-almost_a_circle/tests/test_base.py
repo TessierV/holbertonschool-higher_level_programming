749608cf83ab09aa3d9fe4c13b1b850c10f4a94c
TessierV <5628@holbertonstudents.com>
2023-02-17 06:41:21 -0800

    test

---
+++ python-almost_a_circle/tests/test_base.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Test class base"""
+#!/usr/bin/python3
+"""Test class base"""
+
+
+import unittest
+from models.base import Base
+
+
+class testbase(unittest.TestCase):
+    """Class test base"""
+
+    def test_id(self):
+        base = Base()
+        self.assertEqual(base.id, 1)
+
+    def test_id_prev(self):
+        base = Base()
+        self.assertEqual(base.id, 2)
+
+    def test_id_pass(self):
+        base = Base(89)
+        self.assertEqual(base.id, 89)
+
+if __name__ == '__main__':
+    unittest.main()
