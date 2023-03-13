b2bd440040f12da428ea241a8e77c67917394a59
TessierV <5628@holbertonstudents.com>
2023-03-13 03:40:57 -0700

    test

---
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,7 @@
+-- Create a new database called 'DatabaseName'
+-- Connect to the 'master' database to run this snippet
+USE master
+GO
+-- Create the new database if it does not exist already
+IF NOT EXISTS hbtn_0c_0;
+CREATE DATABASE
