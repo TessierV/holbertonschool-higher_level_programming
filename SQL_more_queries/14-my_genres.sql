013255a8f4acde1ab79a902d255a6508458f02fd
TessierV <5628@holbertonstudents.com>
2023-03-14 06:55:34 -0700

    change the commentary

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,4 +1,4 @@
--- displays the number of records with id = 89
+-- script that uses the hbtn_0d_tvshows database to lists all genres of the show Dexter
 SELECT tv_genres.name
 FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
