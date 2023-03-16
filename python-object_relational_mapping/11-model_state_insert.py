974ff31c9f2acdc7c6e77a9b1957ac6c2f79d0e2
TessierV <5628@holbertonstudents.com>
2023-03-16 08:26:39 -0700

    script

---
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Adds the State object “Louisiana”
+"""
+
+
+import sys
+from sqlalchemy.ext.declarative import declarative_base
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    new_states = State()
+    new_states.name = 'Louisiana'
+    session.add(new_states)
+    result = session.query(State).filter(
+        State.name.like(new_states.name)).first()
+
+    print(f'{result.id}')
+
+    session.commit()
+    session.close()
