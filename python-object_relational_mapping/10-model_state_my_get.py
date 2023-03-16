6f706830d749c852562cd3439c37628c20527ac2
TessierV <5628@holbertonstudents.com>
2023-03-16 08:11:27 -0700

    script 10

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+State object with the name passed as argument
+"""
+
+
+import sys
+from sqlalchemy.ext.declarative import declarative_base
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    result = session.query(State).filter(State.name.like(sys.argv[4])
+                                         ).order_by(State.id)
+
+    for i in result:
+        print(f'{i.id}')
+    session.commit()
+    session.close()
