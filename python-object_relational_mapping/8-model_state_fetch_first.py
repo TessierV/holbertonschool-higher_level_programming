71902b746f17a12426e7a8309f0a9064e1561798
TessierV <5628@holbertonstudents.com>
2023-03-16 07:40:20 -0700

    script 8

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -26,7 +26,7 @@ if __name__ == "__main__":
     if states is not None:
         print(f'{states.id}: {states.name}')
     else:
-        print('Nothing\n')
+        print('Nothing')
 
     session.commit()
     session.close()
