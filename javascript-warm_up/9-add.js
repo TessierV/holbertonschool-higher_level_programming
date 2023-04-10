cc8edb52f0df5708a43f1790668ed2b90367f6d3
TessierV <5628@holbertonstudents.com>
2023-04-10 01:22:00 -0700

    fix error

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -4,11 +4,11 @@ const a = parseInt(process.argv[2]);
 const b = parseInt(process.argv[3]);
 
 function add (a, b) {
-  return a + b;
+  return (a + b);
 }
 
 if (isNaN(a) & isNaN(b)) {
   console.log('NaN');
 } else {
-  console.log(a, b);
+  console.log(add(a, b));
 }
