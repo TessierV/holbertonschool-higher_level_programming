0779056c6e0d5b4f01fd3d3404456c2475296210
TessierV <5628@holbertonstudents.com>
2023-04-10 01:37:24 -0700

    fix send other exercice

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,5 @@
-#!/usr/bin/python3
+#!/usr/bin/node
+
+const numbers = process.argv.slice(2).map(Number);
+const sorted = numbers.sort((a, b) => b - a);
+console.log(sorted[1] || 0);
--- javascript-warm_up/12-object.js
+++ javascript-warm_up/12-object.js
@@ -1,1 +1,9 @@
-#!/usr/bin/python3
+#!/usr/bin/node
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+/* My code */
+myObject.value = 89;
+console.log(myObject);
