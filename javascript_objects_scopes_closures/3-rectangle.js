7b8b21417c9b2ea310ff28179dc7383f65aa6a30
TessierV <5628@holbertonstudents.com>
2023-04-10 03:02:22 -0700

    fix standard

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -1,22 +1,23 @@
 #!/usr/bin/node
 class Rectangle {
-    constructor (w, h) {
-        if (parseInt(w) > 0 && parseInt(h) > 0) {
-            this.width = w;
-            this.height = h;
-        }
+  constructor (w, h) {
+    if (parseInt(w) > 0 && parseInt(h) > 0) {
+      this.width = w;
+      this.height = h;
     }
-    print () {
-        for (let i = 0; i < this.height; i++) {
-          let myVar = '';
-          let y = 0;
-          while (y < this.width) {
-            myVar += 'X';
-            y++;
-          }
+  }
 
-          console.log(myVar);
-        }
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let myVar = '';
+      let y = 0;
+      while (y < this.width) {
+        myVar += 'X';
+        y++;
+      }
+
+      console.log(myVar);
     }
+  }
 }
 module.exports = Rectangle;
