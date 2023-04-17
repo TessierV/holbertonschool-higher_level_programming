b360bc0e89d15b355b9e90a6d4a7783d73d3fa2b
TessierV <5628@holbertonstudents.com>
2023-04-17 00:40:10 -0700

    script 2 fix pep

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -3,7 +3,9 @@ const request = require('request');
 const url = process.argv[2];
 
 request.get(url, (error, response) => {
-    if (error)
-        console.log(error);
+  if (error) {
+    console.log(error);
+  } else {
     console.log(`code: ${response.statusCode}`);
+  }
 });
